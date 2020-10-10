2062621e97fbe74ce46620468d56975f31092cc1
merg13 <mitchellergen@gmail.com>
2020-10-10 10:24:47 -0500

    added submitForm mock functionality stored in mem

--- src/app/travel-request-form/travel-request-form.component.ts
+++ src/app/travel-request-form/travel-request-form.component.ts
@@ -11,10 +11,12 @@ export class TravelRequestFormComponent implements OnInit {
   @Input() model: ITravelRequest;
   numbers: number[] = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 ];
 
+  mockRequests: ITravelRequest [];
+
   constructor() {
     this.model = JSON.parse(localStorage.getItem('TravelRequest'));
 
-    // fake model for debugging
+    // fake model and array of requests for debugging
     this.model = new ITravelRequest();
   }
 
@@ -22,7 +24,24 @@ export class TravelRequestFormComponent implements OnInit {
   }
 
   calculateTotalCost(e: Event): void {
-    this.model.totalCost = (this.model.numTravelers * this.model.arbitraryCost) ?? 0.0;
+
+    try {
+      this.model.totalCost = (this.model.numTravelers * this.model.arbitraryCost) ?? 0.0;
+    }
+    catch(e) {
+      // throw new exception('unable to calculate total cost', e);
+      console.log(e);
+    }
+  }
+
+  submitForm(e: Event): void {
+
+    if (this.mockRequests != null) {
+      this.mockRequests.push(this.model);
+    }
+    else {
+      this.mockRequests = [this.model];
+    }
   }
 
 }
