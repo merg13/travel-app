40f2aef528bbc1fdee424613d225b994d3d20b52
Mitchell Ergen <mitchell.ergen@wolterskluwer.com>
2020-10-22 12:37:19 -0500

    hw3 submission

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,28 @@ import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { TravelRequestFormComponent } from './travel-request-form/travel-request-form.component';
 import { FormsModule } from '@angular/forms';
+import { AppNavComponent } from './app-nav/app-nav.component';
+import { AboutComponent } from './about/about.component';
+import { RouterModule } from '@angular/router';
+import { AppFooterComponent } from './app-footer/app-footer.component';
 
+
+const routes = [
+  { path: 'About', component: AboutComponent },
+  { path: 'TravelRequest', component: TravelRequestFormComponent }
+]
 @NgModule({
   declarations: [
     AppComponent,
-    TravelRequestFormComponent
+    TravelRequestFormComponent,
+    AppNavComponent,
+    AboutComponent,
+    AppFooterComponent
   ],
 imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
